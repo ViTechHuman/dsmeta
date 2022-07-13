a4095b06ccd91297a31c6893b0ba63a116126a63
Victor Marim <victor.ekairos@gmail.com>
2022-07-13 18:42:49 -0300

    Security config

--- frontend/src/componentes/SalesCard/index.tsx
+++ frontend/src/componentes/SalesCard/index.tsx
@@ -49,7 +49,7 @@ function SalesCard() {
             <tr>
               <td className="show992">#341</td>
               <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
+              <td>Graziele</td>
               <td className="show992">15</td>
               <td className="show992">11</td>
               <td>R$ 55300.00</td>
@@ -62,7 +62,7 @@ function SalesCard() {
             <tr>
               <td className="show992">#341</td>
               <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
+              <td>Graziele</td>
               <td className="show992">15</td>
               <td className="show992">11</td>
               <td>R$ 55300.00</td>
@@ -75,7 +75,7 @@ function SalesCard() {
             <tr>
               <td className="show992">#341</td>
               <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
+              <td>Graziele</td>
               <td className="show992">15</td>
               <td className="show992">11</td>
               <td>R$ 55300.00</td>
